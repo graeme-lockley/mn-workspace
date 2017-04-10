1def5b9d3afa111652be187ba24dd0f79d9da320
Graeme Lockley <graemel@no9.co.za>
2017-04-10 21:19:54 +0200

    refactor: curried the Tuple constructor

--- Data/Tuple.js
+++ Data/Tuple.js
@@ -4,21 +4,21 @@ function TupleState(first, second) {
 }
 
 
-function Tuple(first, second) {
-    return new TupleState(first, second);
+function Tuple(first) {
+	return second => new TupleState(first, second);
 }
 
 
 TupleState.prototype.mapFirst = function (f) {
-    return Tuple(f(this.first), this.second);
+    return Tuple(f(this.first))(this.second);
 };
-assumption(Tuple("Hello", 2).mapFirst(x => x + x).first === "HelloHello");
+assumption(Tuple("Hello")(2).mapFirst(x => x + x).first === "HelloHello");
 
 
 TupleState.prototype.mapSecond = function (f) {
-    return Tuple(this.first, f(this.second));
+    return Tuple(this.first)(f(this.second));
 };
-assumption(Tuple("Hello", 2).mapSecond(x => x + x).second === 4);
+assumption(Tuple("Hello")(2).mapSecond(x => x + x).second === 4);
 
 
 module.exports = Tuple;
