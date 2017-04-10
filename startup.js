6d999766baa5fed63e737de2dbbe0ed185ebbbe9
Graeme Lockley <graemel@no9.co.za>
2017-04-10 06:47:15 +0200

    feature: auto-imported Data.String as I need the String prototype updated

--- startup.js
+++ startup.js
@@ -1,7 +1,8 @@
 const Prelude = require("./lib/Prelude");
-
 global.mrequire = Prelude.mrequire;
 
 global.assumption = mrequire("core:assumption:v1.0.0");
+
+const String = require("./Data/String");
 
 module.exports = {};
