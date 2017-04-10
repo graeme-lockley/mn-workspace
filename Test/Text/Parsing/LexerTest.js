a8025fb6fcc4d48a6e3dfcff3643bc0baedd5333
Graeme Lockley <graemel@no9.co.za>
2017-04-10 06:22:21 +0200

    feature: regular expression based lexer

---
+++ Test/Text/Parsing/LexerTest.js
@@ -0,0 +1,57 @@
+const Test = require("./../../Unit");
+
+const Int = require("../../../Data/Int");
+const Tuple = require("../../../Data/Tuple");
+
+const Assert = require("assert");
+
+const Lexer = require("../../../Text/Parsing/Lexer");
+
+
+Test.newSuite("Lexer Suite")
+    .case("given an empty lexer should be at EOF", () => {
+        const lexerDefinition = Lexer.setup({
+            eof: {id: 0}
+        });
+
+        Assert.equal(lexerDefinition.fromString("").token().id, 0);
+    })
+
+    .case("given a lexer with a defined token should return that token", () => {
+        const lexerDefinition = Lexer.setup({
+            eof: {id: 0},
+            tokenPatterns: [
+                Tuple(/[0-9]+/iy, text => ({id: 1, value: Int.fromString(text).withDefault(0)}))
+            ]
+        });
+        const lexer = lexerDefinition.fromString("2912 hello");
+
+        Assert.equal(lexer.token().id, 1);
+        Assert.equal(lexer.token().value, 2912);
+        Assert.deepEqual(lexer.position(), Tuple(1, 0));
+        Assert.equal(lexer.index(), 0);
+    })
+
+    .case("given a lexer with a defined token should return that token and the next token whilst skipping whitespace", () => {
+        const lexerDefinition = Lexer.setup({
+            eof: {id: 0},
+            whitespacePattern: /\s*/iy,
+            tokenPatterns: [
+                Tuple(/[0-9]+/iy, text => ({id: 1, value: Int.fromString(text).withDefault(0)})),
+                Tuple(/[A-Za-z_][A-Za-z0-9_]*/iy, text => ({id: 2, value: text}))
+            ]
+        });
+        const lexer = lexerDefinition.fromString("2912 hello");
+
+        Assert.equal(lexer.token().id, 1);
+        Assert.equal(lexer.token().value, 2912);
+        Assert.deepEqual(lexer.position(), Tuple(1, 0));
+        Assert.equal(lexer.index(), 0);
+
+        const nextLexer = lexer.next();
+
+        Assert.equal(nextLexer.token().id, 2);
+        Assert.equal(nextLexer.token().value, "hello");
+        Assert.deepEqual(nextLexer.position(), Tuple(6, 0));
+        Assert.equal(nextLexer.index(), 5);
+    });
---
+++ Text/Parsing/Lexer.js
@@ -0,0 +1,133 @@
+const Maybe = mrequire("core:lang.Maybe:v1.0.0");
+const Tuple = require("../../Data/Tuple");
+
+
+function TokenState(configuration, state) {
+    this.configuration = configuration;
+    this.state = state;
+}
+
+
+TokenState.prototype.token = function () {
+    return this.state.token;
+};
+
+
+TokenState.prototype.position = function () {
+    return this.state.oldPosition;
+};
+
+
+TokenState.prototype.index = function () {
+    return this.state.oldIndex;
+};
+
+
+TokenState.prototype.next = function () {
+    if (isEndOfFile(this.state.index, this.state.input)) {
+        return new TokenState(this.configuration, finalState(this.configuration.eof, this.state));
+    } else {
+        const currentState = skipWhitespace(this.configuration.whitespacePattern, this.state);
+
+        for (const tokenPatternIndex in this.configuration.tokenPatterns) {
+            const tokenPattern = this.configuration.tokenPatterns[tokenPatternIndex];
+            tokenPattern.first.lastIndex = currentState.index;
+            const regexResult = tokenPattern.first.exec(currentState.input);
+
+            if (regexResult) {
+                return new TokenState(this.configuration, advanceState(currentState, regexResult[0], tokenPattern.second(regexResult[0])));
+            }
+        }
+
+        return this.configuration.error;
+    }
+};
+
+
+
+function isEndOfFile(index, input) {
+    return index >= input.length;
+}
+
+
+function skipWhitespace(whitespaceRegex, state) {
+    if (whitespaceRegex) {
+        const matchedRegex = matchRegex(whitespaceRegex, state);
+        if (matchedRegex) {
+            return advanceState(state, matchedRegex);
+        } else {
+            return state;
+        }
+    } else {
+        return state;
+    }
+}
+
+
+function matchRegex(regex, state) {
+    regex.lastIndex = state.index;
+    const matchedRegex = regex.exec(state.input);
+    if (matchedRegex) {
+        return matchedRegex[0];
+    } else {
+        return undefined;
+    }
+}
+
+
+function advanceState(currentState, matchedText, matchedToken) {
+    let index = currentState.index;
+    let position = currentState.position;
+
+    for (let matchedTextIndex = 0; matchedTextIndex < matchedText.length; matchedTextIndex += 1) {
+        index += 1;
+        if (matchedText.charCodeAt(matchedTextIndex) === 10) {
+            position = position.mapFirst(x => 1).mapSecond(y => y + 1);
+        } else {
+            position = position.mapFirst(x => x + 1);
+        }
+    }
+
+    return {
+        input: currentState.input,
+        index: index,
+        position: position,
+        token: matchedToken,
+        oldPosition: currentState.position,
+        oldIndex: currentState.index
+    };
+}
+
+
+function initialState(input) {
+    return {
+        input: input,
+        position: Tuple(1, 0),
+        index: 0
+    };
+}
+assumption(initialState("Hello world").input === "Hello world" && initialState("Hello world").index === 0);
+
+
+function finalState(eofToken, state) {
+    return {
+        input: state.input,
+        position: state.position,
+        index: state.input.length,
+        token: eofToken
+    };
+}
+
+
+function setup(configuration) {
+    return {
+        fromString: function (input) {
+            return new TokenState(configuration, initialState(input)).next();
+        }
+    };
+}
+
+
+module.exports = {
+    setup
+};
