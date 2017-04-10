d5cc20ca10052d165be88313290c6bcf05a95816
Graeme Lockley <graemel@no9.co.za>
2017-04-10 21:20:22 +0200

    fix: load the Array module at startup as the overloading on arrays is needed

--- startup.js
+++ startup.js
@@ -3,6 +3,7 @@ global.mrequire = Prelude.mrequire;
 
 global.assumption = mrequire("core:assumption:v1.0.0");
 
+const Array = require("./Data/Array");
 const String = require("./Data/String");
 
 module.exports = {};
