258fd5dd7cd736126de4c2ff6d9801071e27fc3d
Graeme Lockley <graemel@no9.co.za>
2017-04-10 06:14:40 +0200

    feature: add fromString into Data.Int

---
+++ Data/Int.js
@@ -0,0 +1,21 @@
+const Maybe = mrequire("core:lang.Maybe:v1.0.0");
+
+
+//- Reads an Int from a `String` value. The number must parse as an integer and fall within the valid range of values
+//- for the `Int` type, otherwise `Nothing` is returned.
+//-
+//- fromString :: String -> Maybe Int
+function fromString(s) {
+    const result = parseInt(s);
+
+    if (isNaN(result)) {
+        return Maybe.Nothing;
+    } else {
+        return Maybe.Just(result);
+    }
+}
+
+
+module.exports = {
+    fromString
+};
