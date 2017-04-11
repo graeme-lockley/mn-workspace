d4bed3bbbb183ce124eefcd2e4e2957162b461bc
Graeme Lockley <graemel@no9.co.za>
2017-04-11 08:08:23 +0200

    refactor: simplified tests

--- Test/Text/Parsing/LexerTest.js
+++ Test/Text/Parsing/LexerTest.js
@@ -8,85 +8,65 @@ const Assert = require("assert");
 const Lexer = require("../../../Text/Parsing/Lexer");
 
 
+const lexerDefinition = Lexer.setup({
+    eof: {id: 0, value: ""},
+    err: text => ({id: -1, value: text}),
+    whitespacePattern: /\s*/iy,
+    tokenPatterns: [
+        Tuple(/[0-9]+/iy)(text => ({id: 1, value: Int.fromString(text).withDefault(0)})),
+        Tuple(/[A-Za-z_][A-Za-z0-9_]*/iy)(text => ({id: 2, value: text}))
+    ].toArray()
+});
+
 Test.newSuite("Lexer Suite")
     .case("given an empty lexer should be at EOF", () => {
-        const lexerDefinition = Lexer.setup({
-            eof: {id: 0},
-            err: text => ({id: -1, value: text})
-        });
+        const lexer = lexerDefinition.fromString("");
 
-        Assert.equal(lexerDefinition.fromString("").token().id, 0);
+        assertLexerState(
+            lexer,
+            0, "", Tuple(1)(0), 0);
     })
 
     .case("given a lexer with a defined token should return that token", () => {
-        const lexerDefinition = Lexer.setup({
-            eof: {id: 0},
-            err: text => ({id: -1, value: text}),
-            tokenPatterns: [
-                Tuple(/[0-9]+/iy)(text => ({id: 1, value: Int.fromString(text).withDefault(0)}))
-            ].toArray()
-        });
         const lexer = lexerDefinition.fromString("2912 hello");
 
-        Assert.equal(lexer.token().id, 1);
-        Assert.equal(lexer.token().value, 2912);
-        Assert.deepEqual(lexer.position(), Tuple(1)(0));
-        Assert.equal(lexer.index(), 0);
+        assertLexerState(
+            lexer,
+            1, 2912, Tuple(1)(0), 0);
     })
 
     .case("given a lexer with a defined token should return that token and the next token whilst skipping whitespace", () => {
-        const lexerDefinition = Lexer.setup({
-            eof: {id: 0},
-            err: text => ({id: -1, value: text}),
-            whitespacePattern: /\s*/iy,
-            tokenPatterns: [
-                Tuple(/[0-9]+/iy)(text => ({id: 1, value: Int.fromString(text).withDefault(0)})),
-                Tuple(/[A-Za-z_][A-Za-z0-9_]*/iy)(text => ({id: 2, value: text}))
-            ].toArray()
-        });
         const lexer = lexerDefinition.fromString("2912 hello");
 
-        Assert.equal(lexer.token().id, 1);
-        Assert.equal(lexer.token().value, 2912);
-        Assert.deepEqual(lexer.position(), Tuple(1)(0));
-        Assert.equal(lexer.index(), 0);
+        assertLexerState(
+            lexer,
+            1, 2912, Tuple(1)(0), 0);
 
-        const nextLexer = lexer.next();
-
-        Assert.equal(nextLexer.token().id, 2);
-        Assert.equal(nextLexer.token().value, "hello");
-        Assert.deepEqual(nextLexer.position(), Tuple(6)(0));
-        Assert.equal(nextLexer.index(), 5);
+        assertLexerState(
+            lexer.next(),
+            2, "hello", Tuple(6)(0), 5);
     })
 
     .case("given a lexer with an character that the lexer does not recognise then the error token is returned and the lexer is advanced onto the next character", () => {
-        const lexerDefinition = Lexer.setup({
-            eof: {id: 0},
-            err: text => ({id: -1, value: text}),
-            whitespacePattern: /\s*/iy,
-            tokenPatterns: [
-                Tuple(/[0-9]+/iy)(text => ({id: 1, value: Int.fromString(text).withDefault(0)})),
-                Tuple(/[A-Za-z_][A-Za-z0-9_]*/iy)(text => ({id: 2, value: text}))
-            ].toArray()
-        });
         const lexer = lexerDefinition.fromString("2912*hello");
 
-        Assert.equal(lexer.token().id, 1);
-        Assert.equal(lexer.token().value, 2912);
-        Assert.deepEqual(lexer.position(), Tuple(1)(0));
-        Assert.equal(lexer.index(), 0);
+        assertLexerState(
+            lexer,
+            1, 2912, Tuple(1)(0), 0);
 
-        const nextLexer = lexer.next();
+        assertLexerState(
+            lexer.next(),
+            -1, "*", Tuple(5)(0), 4);
 
-        Assert.equal(nextLexer.token().id, -1);
-        Assert.equal(nextLexer.token().value, "*");
-        Assert.deepEqual(nextLexer.position(), Tuple(5)(0));
-        Assert.equal(nextLexer.index(), 4);
-
-        const nextNextLexer = nextLexer.next();
-
-        Assert.equal(nextNextLexer.token().id, 2);
-        Assert.equal(nextNextLexer.token().value, "hello");
-        Assert.deepEqual(nextNextLexer.position(), Tuple(6)(0));
-        Assert.equal(nextNextLexer.index(), 5);
+        assertLexerState(
+            lexer.next().next(),
+            2, "hello", Tuple(6)(0), 5);
     });
+
+
+function assertLexerState(lexer, id, value, position, index) {
+    Assert.equal(lexer.token().id, id);
+    Assert.equal(lexer.token().value, value);
+    Assert.deepEqual(lexer.position(), position);
+    Assert.equal(lexer.index(), index);
+}
