aba5a521c1909013960ad6889dbb366459cedcb3
Graeme Lockley <graemel@no9.co.za>
2017-04-14 10:02:39 +0200

    refactor: remove array and string import as the duck typing strategy has been dropped

--- startup.js
+++ startup.js
@@ -3,7 +3,4 @@ global.mrequire = Prelude.mrequire;
 
 global.assumption = mrequire("core:assumption:v1.0.0");
 
-const Array = require("./Data/Array");
-const String = require("./Data/String");
-
 module.exports = {};
