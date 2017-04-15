152b1c1a28c9e556671b461cc88afdcf7d10465a
Graeme Lockley <graemel@no9.co.za>
2017-04-15 11:45:36 +0200

    feature: change implementation to state based rather than function based

--- Data/Result.js
+++ Data/Result.js
@@ -2,22 +2,50 @@
 
 const Maybe = mrequire("core:Data.Maybe:v1.0.0");
 
+const identity = x => x;
+
+const constant = c => _ => c;
+
+
+function Result(value) {
+    this.value = value;
+}
+
 
 function Okay(value) {
-    return [0, value];
+    return new Result([0, value]);
 }
 
 
 function Error(value) {
-    return [1, value];
+    return new Result([1, value]);
 }
 
 
-function withDefault(value) {
-    return result => result[0] === 0 ? result[1] : value;
-}
-assumption(withDefault(1)(Okay(10)) === 10);
-assumption(withDefault(1)(Error(10)) === 1);
+Result.prototype.map = function(okayMap) {
+    return errorMap => this.value[0] === 0 ? okayMap(this.value[1]) : errorMap(this.value[1]);
+};
 
 
-module.exports = {Error, Okay, withDefault};
+Result.prototype.withDefault = function (value) {
+    return this.map(identity)(constant(value));
+};
+assumption(Okay(10).withDefault(1) === 10);
+assumption(Error(10).withDefault(1) === 1);
+
+
+Result.prototype.isOkay = function() {
+    return this.map(constant(true))(constant(false));
+};
+assumption(Okay(10).isOkay());
+assumption(!Error(10).isOkay());
+
+
+Result.prototype.isError = function() {
+    return this.map(constant(false))(constant(true));
+};
+assumption(!Okay(10).isError());
+assumption(Error(10).isError());
+
+
+module.exports = {Error, Okay};
