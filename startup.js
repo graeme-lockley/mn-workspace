89ea401a8c55782030a97dbb53b706e0a9379f27
Graeme Lockley <graemel@no9.co.za>
2017-04-16 11:34:41 +0200

    feature: add support for assumptionEqual

--- startup.js
+++ startup.js
@@ -1,6 +1,9 @@
 const Prelude = require("./lib/Prelude");
 global.mrequire = Prelude.mrequire;
 
-global.assumption = mrequire("core:assumption:v1.0.0");
+const Assumption = mrequire("core:assumption:v1.0.0");
+
+global.assumption = Assumption.assumption;
+global.assumptionEqual = Assumption.assumptionEqual;
 
 module.exports = {};
