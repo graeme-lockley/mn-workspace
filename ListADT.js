90b4c74a663f94911f147893cb651c27ba6fcd18
Graeme Lockley <graemel@no9.co.za>
2017-04-20 06:35:49 +0200

    feature: illustrative example of using a tool to generate code

---
+++ ListADT.js
@@ -0,0 +1,12 @@
+const State = use("./ListADTState.1.0.0.adt");
+
+
+State.ListADTState.prototype.length = function () {
+	return this.reduce(() => 0)(head => tail => 1 + tail.length());
+};
+
+
+module.exports = {
+	Nil: State.Nil,
+	Cons: State.Cons
+};
