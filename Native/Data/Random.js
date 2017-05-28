f40878f3119e3de8d5e6739cdd4a84c20e2399dd
Graeme Lockley <graemel@no9.co.za>
2017-05-28 12:25:19 +0200

    feature: a simple and crude pseudo random number generator

---
+++ Native/Data/Random.js
@@ -0,0 +1,37 @@
+const random = () =>
+    new Promise(accept => accept(Math.random()));
+
+
+function RandomType(content) {
+    this.content = content;
+}
+
+
+RandomType.prototype.next = function () {
+    return new RandomType(this.content * 16807 % 2147483647);
+};
+
+
+RandomType.prototype.asNativeInt = function() {
+    return this.content;
+};
+
+
+RandomType.prototype.asFloat = function () {
+    return (this.content - 1) / 2147483646;
+};
+
+
+const Random = seed => {
+    const newSeed = seed % 2147483647;
+
+    return (newSeed <= 0)
+        ? new RandomType(newSeed + 2147483646)
+        : new RandomType(newSeed);
+};
+
+
+module.exports = {
+    Random,
+    random
+};
